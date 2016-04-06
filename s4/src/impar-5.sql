3db34d2be80c1b34b056237514899cc8f4eb5bda
Parham Alvani <parham.alvani@gmail.com>
2016-04-06 14:19:16 +0430

    Session 4 exercise 5 was added.

---
+++ s4/src/impar-5.sql
@@ -0,0 +1,90 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : impar-5.sql
+ *
+ * [] Creation Date : 06-04-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+create table PostalCode (
+	FirstName varchar(50),
+	LastName varchar(50),
+	PostalCode integer
+);
+
+insert into values (
+	'Michael',
+	'Blythe',
+	98027
+);
+
+insert into values (
+	'Linda',
+	'Mitchell',
+	98027
+);
+
+insert into values (
+	'Jillian',
+	'Carson',
+	98027
+);
+
+insert into values (
+	'Garrett',
+	'Vargas',
+	98027
+);
+
+insert into values (
+	'Tsvi',
+	'Reiter',
+	98027
+);
+
+insert into values (
+	'Shu',
+	'Ito',
+	98055
+);
+
+insert into values (
+	'Jose',
+	'Saraiva',
+	98055
+);
+
+insert into values (
+	'Tete',
+	'Mensa-Annan',
+	98055
+);
+
+insert into values (
+	'Lynn',
+	'Tsofilias',
+	98055
+);
+
+insert into values (
+	'Rachel',
+	'Valdez',
+	98055
+);
+
+insert into values (
+	'Jae',
+	'Pak',
+	98055
+);
+
+insert into values (
+	'Ranjit',
+	'Varkey Chudukatil',
+	98055
+);
