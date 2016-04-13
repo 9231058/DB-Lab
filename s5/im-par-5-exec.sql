bd0afa1e600e456311884691459a96ed834907fa
Parham Alvani <parham.alvani@gmail.com>
2016-04-13 14:19:56 +0430

    Problem 5 of session 5 was added.

---
+++ s5/im-par-5-exec.sql
@@ -0,0 +1,14 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : im-par-5-exec.sql
+ *
+ * [] Creation Date : 13-04-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+delete * from "Book-1" where "Book-1".ID = 1;
